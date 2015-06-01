d42052950d7d8cafdfb44e55cf7ffc3c71810482
王卓 <280034816@qq.com>
2015-06-01 13:34:46 +0900

    Fix Bugs
    修复bug并增加了运算符变色的功能

--- Final_calculator/ViewController.swift
+++ Final_calculator/ViewController.swift
@@ -22,6 +22,9 @@ class ViewController: UIViewController {
         var rightValue:Double?
         //是否开始计算，true表示进行右值的设置，false 处于左值阶段
         var isCalc:Bool = false
+        //上一个按过的按钮
+        var LastTappedButton:UIButton?
+    
 
         //需要写一个而方法，来统一处理数值的输入
         //按钮0点击
@@ -40,6 +43,11 @@ class ViewController: UIViewController {
         //我们需要通过isCalc这个变量来进行判断，是更新左值还是右值
         func onAddNum(num:Int){
             if !isCalc {
+                //########################
+                if strResult == "0."{
+                    leftValue=0
+                }
+                //########################
                 //当处于左值阶段，同时左值又是nil的时候，需要将文本标签的字符串变为0
                 if leftValue == nil {
                     strResult = "0"
@@ -59,13 +67,31 @@ class ViewController: UIViewController {
             labResult.text=strResult
         }
         func onSetNum(num:Int){
-            if strResult == "0" {
+            if strResult == "0."{
+                strResult += "\(num)"
+            }
+            else if strResult == "0" {
                 strResult = "\(num)"
             }else{
                 //不为0的时候，将字符串进行连接。
                 strResult += "\(num)"
             }
         }
+    
+        //清除上一个计算按钮的颜色
+        func ClearColor() {
+            if var lbtn=LastTappedButton{
+            lbtn.backgroundColor=UIColor(red: 255/255, green: 195/255, blue: 62/255, alpha: 1)
+            //lbtn.backgroundColor=UIColor.redColor()
+            }
+        }
+    
+        //
+    func SetColor(sender:UIButton){
+            sender.backgroundColor=UIColor(red: 188/255, green: 1, blue: 44/255, alpha: 1)
+            //sender.backgroundColor=UIColor.orangeColor()
+        }
+    
         //按钮清除点击
         @IBAction func btnCTap() {
             strResult = "0"
@@ -73,6 +99,7 @@ class ViewController: UIViewController {
             leftValue = nil
             rightValue = nil
             labResult.text=strResult
+            ClearColor();
         }
         //按钮正负反转点击
         @IBAction func btnNTap() {
@@ -87,25 +114,48 @@ class ViewController: UIViewController {
             labResult.text=strResult
         }
         //按钮除点击
-        @IBAction func btnDTap() {
+        @IBAction func btnDTap(sender:UIButton) {
+            if isCalc{
+                btnRTap()
+            }
             onToRight()
             type = CalcType.Divide
-            
+            ClearColor()
+            SetColor(sender)
+            LastTappedButton=sender
         }
         //按钮乘点击
-        @IBAction func btnMTap() {
+        @IBAction func btnMTap(sender:UIButton) {
+            if isCalc{
+                btnRTap()
+            }
             onToRight()
             type = CalcType.Multiply
+            ClearColor()
+            SetColor(sender)
+            LastTappedButton=sender
         }
         //按钮减点击
-        @IBAction func btnSTap() {
+        @IBAction func btnSTap(sender:UIButton) {
+            if isCalc{
+                btnRTap()
+            }
             onToRight()
             type = CalcType.Subtract
+            ClearColor()
+            SetColor(sender)
+            LastTappedButton=sender
         }
         //按钮加点击
-        @IBAction func btnATap() {
+        @IBAction func btnATap(sender:UIButton) {
+            if isCalc{
+                btnRTap()
+            }
             onToRight()
             type = CalcType.Add
+            ClearColor()
+            SetColor(sender)
+            LastTappedButton=sender
         }
         
         //转换到右值设置
@@ -121,29 +171,35 @@ class ViewController: UIViewController {
         @IBAction func btnRTap() {
             //当算式进行到右值设置时，这时候才执行运算，同时将进度恢复到左值设置
             if isCalc {
-                onExecute(rightValue!)
+                onExecute(rightValue)
                 isCalc = false
             }
-            
+            ClearColor()
         }
         //计算方法
-        func onExecute(newValue:Double){
+        func onExecute(newValue:Double?){
+        
+            if var RightValue=newValue{
             switch type
             {
             case .Add:
-                leftValue! += newValue
+                leftValue! += RightValue
             case .Subtract:
-                leftValue! -= newValue
+                leftValue! -= RightValue
             case .Multiply:
-                leftValue! *= newValue
+                leftValue! *= RightValue
             case .Divide:
-                leftValue! /= newValue
+                leftValue! /= RightValue
             default:
                 return
             }
             strResult = ("\(leftValue!)")
             labResult.text=strResult
             leftValue = nil
+            }
+            else {
+                btnCTap()
+            }
         }
         
     //加减乘除四则运算符号
@@ -155,7 +211,6 @@ class ViewController: UIViewController {
         case Add
         case Nil
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -166,4 +221,3 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 }
-
